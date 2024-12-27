12fc5f049481483756939e4ed198d77e4a4d3138
Muhammad Bashir <MuhammadBashir76500@gmail.com>
2024-12-27 12:15:45 +0500

    Add files via upload

---
+++ Project2.py
@@ -0,0 +1,11 @@
+a=2 # Integer datatype
+
+b="Hello World" # String datatype
+
+c=30.8 # Float datatype
+
+d=True or False # Boolean datatype
+
+e=None # None datatype
+
+# f = greater than or equal to # logical datatype
